42e8f65586c34e892614e892b6c86cff4c17be20
yogeshwar0904 <genithayogeshwar@gmail.com>
2023-01-06 18:04:20 +0530

    implement theater crud

--- src/main/java/com/ideas2it/ideas2movie/repository/CastAndCrewRepository.java
+++ src/main/java/com/ideas2it/ideas2movie/repository/CastAndCrewRepository.java
@@ -7,13 +7,7 @@ package com.ideas2it.ideas2movie.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-import com.ideas2it.ideas2movie.model.Theater;
+import com.ideas2it.ideas2movie.model.CastAndCrew;
 @Repository
-public interface TheaterRepository extends JpaRepository<Theater, Long> {
-    boolean existsByTheaterCity(String city);
-    boolean existsByTheaterName(String theatreName);
-
-
+public interface CastAndCrewRepository extends JpaRepository<CastAndCrew, Long> {
 }
