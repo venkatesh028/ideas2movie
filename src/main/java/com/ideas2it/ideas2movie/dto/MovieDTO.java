bff83fbd1b9f0bcba43f9e6639654d77916577d9
yogeshwar0904 <genithayogeshwar@gmail.com>
2023-01-09 17:53:21 +0530

    changes in theater controller

--- src/main/java/com/ideas2it/ideas2movie/dto/MovieDTO.java
+++ src/main/java/com/ideas2it/ideas2movie/dto/MovieDTO.java
@@ -1,4 +1,15 @@
 package com.ideas2it.ideas2movie.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
+import com.ideas2it.ideas2movie.util.constant.Message;
+
 public class MovieDTO {
+    private Long id;
+    @NotBlank(message = Message.DIRECTOR_NAME_SHOULD_NOT_BE_EMPTY)
+    private String director;
+    @NotBlank(message = Message.HERO_NAME_SHOULD_NOT_BE_EMPTY)
+    private String hero;
+    @NotBlank(message = Message.HEROINE_NAME_SHOULD_NOT_BE_EMPTY)
+    private String heroine;
 }
