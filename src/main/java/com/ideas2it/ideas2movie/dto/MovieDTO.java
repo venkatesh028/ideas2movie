1c6138d411552706d42a2219159e11425db8951f
yogeshwar0904 <genithayogeshwar@gmail.com>
2023-01-09 19:35:29 +0530

    Constant for service

--- src/main/java/com/ideas2it/ideas2movie/dto/MovieDTO.java
+++ src/main/java/com/ideas2it/ideas2movie/dto/MovieDTO.java
@@ -2,14 +2,22 @@ package com.ideas2it.ideas2movie.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.time.LocalTime;
+
+import com.ideas2it.ideas2movie.model.CastAndCrew;
 import com.ideas2it.ideas2movie.util.constant.Message;
+import com.ideas2it.ideas2movie.util.enums.Genre;
+import com.ideas2it.ideas2movie.util.enums.Language;
 
 public class MovieDTO {
     private Long id;
-    @NotBlank(message = Message.DIRECTOR_NAME_SHOULD_NOT_BE_EMPTY)
-    private String director;
-    @NotBlank(message = Message.HERO_NAME_SHOULD_NOT_BE_EMPTY)
-    private String hero;
-    @NotBlank(message = Message.HEROINE_NAME_SHOULD_NOT_BE_EMPTY)
-    private String heroine;
+    @NotBlank(message = Message.MOVIE_NAME_SHOULD_NOT_BE_EMPTY)
+    private String name;
+    @NotBlank(message = Message.LANGUAGE_SHOULD_NOT_BE_EMPTY)
+    private Language language;
+    @NotBlank(message = Message.DURATION_SHOULD_NOT_BE_EMPTY)
+    private LocalTime duration;
+    @NotBlank(message = Message.GENRE_SHOULD_NOT_BE_EMPTY)
+    private Genre gener;
+    private CastAndCrew castAndCrew;
 }
--- src/main/java/com/ideas2it/ideas2movie/dto/responsedto/MovieResponseDTO.java
+++ src/main/java/com/ideas2it/ideas2movie/dto/responsedto/MovieResponseDTO.java
@@ -1,5 +1,16 @@
 package com.ideas2it.ideas2movie.dto.responsedto;
 
-public class MovieResponseDTO {
+import java.time.LocalTime;
 
+import com.ideas2it.ideas2movie.model.CastAndCrew;
+import com.ideas2it.ideas2movie.util.enums.Genre;
+import com.ideas2it.ideas2movie.util.enums.Language;
+
+public class MovieResponseDTO {
+    private Long id;
+    private String name;
+    private Language language;
+    private LocalTime duration;
+    private Genre genre;
+    private CastAndCrew castAndCrew;
 }
