57f046966bd785fa6db35f6742aa07f0d94becef
venkatesh <venkateshtm28@gmail.com>
2023-01-12 10:52:46 +0530

    Working in exception

--- src/main/java/com/ideas2it/ideas2movie/exception/NotFoundException.java
+++ src/main/java/com/ideas2it/ideas2movie/exception/NotFoundException.java
@@ -8,8 +8,9 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@Getter
 public class NotFoundException  extends Exception{
+    private static HttpStatus httpStatus = HttpStatus.NOT_FOUND;
     public NotFoundException(String message){
         super(message);
     }
